c7aec8925a305cf62a82eb2019720d6b229e703c
sickk <jaakmyr@gmail.com>
2025-01-01 23:12:55 -0800

    minor ui changes

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Mikutopia",
+    description:"Discover & Share Mikuness"
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient">j</div>
+            </div>
+
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Feed from '@components/feed'
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br />
+            <span className="orange_gradient">Miku Lover's Paradise</span>
+        </h1>
+        <p className="desc text-center">
+            Mikutopia is the worlds next crappy fullstack crud project. but don't worry. there's Hatsune Miku!
+        </p>
+    </section>
+  )
+}
+
+export default Home
